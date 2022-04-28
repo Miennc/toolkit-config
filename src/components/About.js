3659d62314332d4f53320bad7675beb789e6a4d6
Mien <2502mien@gmail.com>
2022-04-28 22:37:40 +0700

    add i18

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
+import { changeLanguage } from 'i18next';
 import React from 'react'
-
+import { useTranslation } from "react-i18next";
 export default function About() {
+  const { t, i18n } = useTranslation();
+
+  const changeLag = (lang) => {
+    i18n.changeLanguage(lang);
+  }
   return (
     <div >
-        this is about
+      <select onChange={(e)=>changeLag(e.target.value)}>
+        <option value="en">English</option>
+        <option value="vi">Vietnamese</option>
+      </select>
+      <h1>{t("content.hello")}</h1>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Provider}  from 'react-redux';
-import {reducers} from "./slices"
+import { Provider } from 'react-redux';
+import { reducers } from "./slices"
 import { configureStore } from '@reduxjs/toolkit';
 import reportWebVitals from './reportWebVitals';
+import i18n from "./translation/i18n";
+import { I18nextProvider } from "react-i18next";
 
 const store = configureStore({
   reducer: reducers
 });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <I18nextProvider i18n={i18n}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
